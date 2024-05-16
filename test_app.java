7e726447aa93dfb81a8675cae1fa21fb1e8025ed
PreetiSharma-348 <preetis@heaptrace.net>
2024-05-16 14:30:25 +0530

    Add files via upload
    This is my first code

---
+++ test_app.java
@@ -0,0 +1,9 @@
+public class Test_app
+{
+    public static void main(String args[])
+    {
+        System.out.println("Hi I am Preeti");
+        System.out.println("Software Developer Trainee");
+        System.out.println("At Heaptrace Technologies");
+    }
+}
